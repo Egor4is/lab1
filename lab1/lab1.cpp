140e47aaa857721a993b89c6fd69acf9f61fef1a
poram <poram@GRINGO>
2026-02-11 15:38:42 +0700

    Добавьте файлы проекта.

---
+++ lab1/lab1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int MAX;
+    cout << "MAX: ";
+    cin >> MAX;
+    int a = 0, b = 1;
+    int S = 0;
+    int N = 0;
+    cout << "Числа: ";
+    while (S <= MAX) {
+        cout << a << " ";
+        S += a;
+        N++;
+        int next = a + b;
+        a = b;
+        b = next;
+    }
+    cout << "N=" << N << " ";
+    cout << "S=" << S << "\n";
+}
